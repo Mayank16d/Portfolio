9f7be570bfaed6214a98c0b7478df92716ebc875
Mayank Nandwani <nandwanimayank007@gmail.com>
2024-06-21 13:36:58 +0530

    minor error

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -18,7 +18,7 @@ function ContactPage() {
                     </div>
                     <div>
                         <label htmlFor="message" className="block text-sm font-medium text-gray-300">Message</label>
-                        <textarea id="message" name="message" rows="4" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"></textarea>
+                        <textarea id="message" name="message" rows={4} className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"></textarea>
                     </div>
                     <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Send Message</button>
                 </form>
